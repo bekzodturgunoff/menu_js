5af13cd698d8424552206adaa5b7794620984e25
Bekzod <bekzodturgunoff@gmail.com>
2022-12-16 11:20:34 +0500

    bug on the btns

--- app.js
+++ app.js
@@ -71,34 +71,28 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "Some food",
+    category: "dinner",
+    price: 18.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
-const fliterBtns = document.querySelectorAll(".filter-btn");
+const container = document.querySelector(".btn-container");
 
 window.addEventListener("DOMContentLoaded", function () {
   displayMenuItems(menu);
-});
-
-fliterBtns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const category = e.currentTarget.dataset.id;
-    const menuCategory = menu.filter(function (menuItem) {
-      if (menuItem.category === category) {
-        return menuItem;
-      }
-    });
-    if (category === "all") {
-      displayMenuItems(menu);
-    } else {
-      displayMenuItems(menuCategory);
-    }
-  });
+  displayMenuButtons();
 });
 
 function displayMenuItems(meunItems) {
   let displayMenu = meunItems.map(function (item) {
     return `
+
       <article class="menu-item">
           <img src=${item.img} alt=${item.title} class="photo" />
 
@@ -116,4 +110,41 @@ function displayMenuItems(meunItems) {
   });
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `
+    <button class="filter-btn" type="button" data-id=${category}>${category}</button
+
+    `;
+    })
+    .join("");
+  container.innerHTML = categoryBtns;
+  const fliterBtns = container.querySelectorAll(".filter-btn");
+  fliterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 }
